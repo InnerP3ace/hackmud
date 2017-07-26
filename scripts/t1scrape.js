9cb6eb423ecc5dda15c34ab3df9f2c2533e6ef97
InnerP3ace <28911506+InnerP3ace@users.noreply.github.com>
2017-07-26 17:01:37 -0400

    optimizations and such
    put most of the work within their own functions

--- scripts/t1scrape.js
+++ scripts/t1scrape.js
@@ -1,22 +1,34 @@
 function(context, args)  // target:#s.t1npc.corp
 {
-	let response = args.target.call();
-	response = response.split("\n").join(" ");
 	let ret = "";
-	let regex = /\s(\w+)\s\|/g;
-	
-	let resultOfMatch = [];
-	let matches = regex.exec(response);
-	while (matches) {
-		if (matches.index === regex.lastIndex) {
-			regex.lastIndex++;
-		}
-		matches.forEach((match, groupIndex) => {
-			if (groupIndex) {
-				resultOfMatch.push(match)
-			}
-		})
-		matches = regex.exec(response);
+	// formats the string to one line, no \n.
+	function formatString(stringToFormat) {
+		return stringToFormat.split("\n").join(" ");
 	}
-	return resultOfMatch;
+	// searches the front page for public access pages.
+	// regex only returns a single match, so it is called
+	// multiple times until it is done.
+	function pubAccessPageSearch(regex, response) {
+		let resultOfMatch = [];
+		let matches = regex.exec(response);
+		while (matches) {
+			if (matches.index === regex.lastIndex) {
+				regex.lastIndex++;
+			}
+			matches.forEach((match, groupIndex) => {
+				if (groupIndex) {
+					resultOfMatch.push(match)
+				}
+			})
+			matches = regex.exec(response);
+		}
+		return resultOfMatch;
+	}
+	// main source of output.
+	function logicController() {
+		let response = formatString(args.target.call());
+		let regex = /\s(\w+)\s\|/g;
+		return pubAccessPageSearch(regex, response);
+	}
+	return logicController();
 }
