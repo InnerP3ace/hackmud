aa36623cd38883515ce89158daa324b8e4b3d6c3
InnerP3ace <28911506+InnerP3ace@users.noreply.github.com>
2017-07-26 16:38:28 -0400

    caught back up
    public pages can now be seen using this,i still have to get the navigation arg.

--- scripts/t1scrape.js
+++ scripts/t1scrape.js
@@ -1,6 +1,22 @@
-function(context, args)
+function(context, args)  // target:#s.t1npc.corp
 {
-	var caller = context.caller;
-	var l = #s.scripts.lib();
-	return { ok:false };
+	let response = args.target.call();
+	response = response.split("\n").join(" ");
+	let ret = "";
+	let regex = /\s(\w+)\s\|/g;
+	
+	let resultOfMatch = [];
+	let matches = regex.exec(response);
+	while (matches) {
+		if (matches.index === regex.lastIndex) {
+			regex.lastIndex++;
+		}
+		matches.forEach((match, groupIndex) => {
+			if (groupIndex) {
+				resultOfMatch.push(match)
+			}
+		})
+		matches = regex.exec(response);
+	}
+	return resultOfMatch;
 }
