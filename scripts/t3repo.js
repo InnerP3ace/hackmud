77e4b3316d0d3a06ef6d35430616b24ee30dfce8
InnerP3ace <28911506+InnerP3ace@users.noreply.github.com>
2017-07-28 19:01:16 -0400

    doesnt work?
    regex was made for finding page names, but for some odd reason doesnt seems to find a match, even though regex101.com says it does

--- scripts/t3repo.js
+++ scripts/t3repo.js
@@ -3,9 +3,25 @@ function(context, args) { // target:#s.t3.corp, username:"username", pin:"4nums"
 	function formatString(stringToFormat) {
 		return stringToFormat.split("\n").join(" ");
 	}
+	// same function as in my t1scrape script
+	function search(regex, response) {
+		let resultOfMatch = [];
+		let matches = regex.exec(response);
+		while (matches) {
+			if (matches.index === regex.lastIndex) {
+				regex.lastIndex++;
+			}
+			resultOfMatch.push(matches[1] || matches[2] || matches[3])
+			matches = regex.exec(response);
+		}
+		return [...new Set(resultOfMatch)];
+	}
 	function logicController() {
+		let regex = /\+\s+(\w+)\s+\+/g;
 		let response = formatString(args.target.call({username:args.username, pin:args.pin}));
-		return response;
+		response = args.target.call({username:args.username, pin:args.pin})
+		let pages = regex.exec(response);
+		return pages;
 	}
 	return logicController();
 }
