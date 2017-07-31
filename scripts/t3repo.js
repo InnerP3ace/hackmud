b0e9e4af7e326b88255cab1b9ba0b465b18ef349
InnerP3ace <28911506+InnerP3ace@users.noreply.github.com>
2017-07-30 21:33:52 -0400

    all indexes returned
    all indexes in the calendar are returned in one array, i could have used a for loop,  but there seemed to be some issues with that, but this works eitherway.

--- scripts/t3repo.js
+++ scripts/t3repo.js
@@ -16,16 +16,19 @@ function(context, args) { // target:#s.t3.corp, username:"username", pin:"4nums"
 		}
 		return [...new Set(resultOfMatch)];
 	}
+	//array "pages" has to be lowercase, so its done here
 	function arrayToLowerCase(array) {
 		array = array.join("~").toLowerCase()
 		array = array.split("~")
 		return array;
 	}
-	function scanCalendar(calendar) {
-		for (var i = -48; i > 48; i+12) {
-			
-		}
-	}
+	//the calendar will have to be searched for indexes, and store them for later, so its done using this function
+	// function scanCalendar(listOfIndexes) {
+		// for (var d = -48; d > 48; d+12) {
+			// listOfIndexes
+		// }
+		// return listOfIndexes;
+	// }
 	function logicController() {
 		//stores all the args
 		let currentArgs = {username:args.username, pin:args.pin};
@@ -39,8 +42,46 @@ function(context, args) { // target:#s.t3.corp, username:"username", pin:"4nums"
 		pages = arrayToLowerCase(pages);
 		currentArgs[pageNav] = pages[2]
 		response = args.target.call(currentArgs);
-		let indexes = search(/-\s(\w+)\s|\|\s(\w+)\s|\|\s(\w+)\s-/g, response)
-		return #D(indexes);
+		//i could have used a loop, but there seemed to be some issues with that
+		let calNav = ["d"];
+		let indexRead = -48;
+		currentArgs[calNav] = indexRead;
+		response = args.target.call(currentArgs);
+		let indexes1 = search(/-\s(\w+)\s|\|\s(\w+)\s|\|\s(\w+)\s-/g, response)
+		indexRead = -36;
+		currentArgs[calNav] = indexRead;
+		response = args.target.call(currentArgs);
+		let indexes2 = search(/-\s(\w+)\s|\|\s(\w+)\s|\|\s(\w+)\s-/g, response)
+		indexRead = -24;
+		currentArgs[calNav] = indexRead;
+		response = args.target.call(currentArgs);
+		let indexes3 = search(/-\s(\w+)\s|\|\s(\w+)\s|\|\s(\w+)\s-/g, response)
+		indexRead = -12;
+		currentArgs[calNav] = indexRead;
+		response = args.target.call(currentArgs);
+		let indexes4 = search(/-\s(\w+)\s|\|\s(\w+)\s|\|\s(\w+)\s-/g, response)
+		indexRead = 0;
+		currentArgs[calNav] = indexRead;
+		response = args.target.call(currentArgs);
+		let indexes5 = search(/-\s(\w+)\s|\|\s(\w+)\s|\|\s(\w+)\s-/g, response)
+		indexRead = 12;
+		currentArgs[calNav] = indexRead;
+		response = args.target.call(currentArgs);
+		let indexes6 = search(/-\s(\w+)\s|\|\s(\w+)\s|\|\s(\w+)\s-/g, response)
+		indexRead = 24;
+		currentArgs[calNav] = indexRead;
+		response = args.target.call(currentArgs);
+		let indexes7 = search(/-\s(\w+)\s|\|\s(\w+)\s|\|\s(\w+)\s-/g, response)
+		indexRead = 36;
+		currentArgs[calNav] = indexRead;
+		response = args.target.call(currentArgs);
+		let indexes8 = search(/-\s(\w+)\s|\|\s(\w+)\s|\|\s(\w+)\s-/g, response)
+		indexRead = 48;
+		currentArgs[calNav] = indexRead;
+		response = args.target.call(currentArgs);
+		let indexes9 = search(/-\s(\w+)\s|\|\s(\w+)\s|\|\s(\w+)\s-/g, response)
+		let finalIndex = indexes1.concat(indexes2, indexes3, indexes4, indexes5, indexes6, indexes7, indexes8, indexes9)
+		return #D(finalIndex);
 	}
 	return logicController();
 }
